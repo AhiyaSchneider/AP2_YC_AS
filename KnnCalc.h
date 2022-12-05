77757d7b035ec1e7b82d17dba192acdf834be4ef
Yair Cohen <yaish81@gmail.com>
2022-12-05 22:00:44 +0200

    .

--- KnnCalc.h
+++ KnnCalc.h
@@ -15,7 +15,8 @@ private:
     int k;               // for holding amuont of neighbors chosen by the user (args[0])
     string inputFile;    // args[1] - file to read neighbors from
     string distanceType; // args[2] - chosen metric
-    list<vector<double>> distanceList;
+    list<vector<double>> vectorList;
+    list<double> distanceList;
 
 public:
     KnnCalc(int j, std::string file, std::string DistanceType);
