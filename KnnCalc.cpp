620cf5b23c9dda69f9074d0e5f22ff4daaaf3f5e
Yair Cohen <yaish81@gmail.com>
2022-12-05 22:00:32 +0200

    .

--- KnnCalc.cpp
+++ KnnCalc.cpp
@@ -27,7 +27,8 @@ using namespace std;
 
     void KnnCalc::findK_NearestNeighbors()
     {
-        distanceList = list<vector<double>>();
+        vectorList = list<vector<double>>();
+        distanceList = list<double>();
         ifstream inFile;
         inFile.open(inputFile);
         if (!inFile.is_open())
@@ -42,14 +43,23 @@ using namespace std;
         int v1Length = calc.getV1().size();
         vector<double> v;
         string s;
-        while (getline(inFile, s))
+        while (getline(inFile, s, ','))
         {
+            try{
             double x = stod(s);
             v.push_back(x);
-            calc.setV2(v);
-            distanceList.push_back(calc.getV2());
+            }
             // todo - a function for adding new vector to the list - mabye generalize setV2 func - for extracting a vector from a file regardless to its destination after.
             //getting the vectros already categories according to type (red/white wine etc.)
+            catch(exception) {
+                if (!v.size() == v1Length) {
+                    exit(1);
+                }
+                calc.setV2(v);
+                // TODO - switch case and validation check
+                vectorList.push_back(v); // need to add name of each vector and link the two lists
+                distanceList.push_back(calc.manhattan_Distance());
+            }
         }
         // todo - private partition function for the whole list of vectors extracted from the file
         // todo - return k first neighbors in the list after partition
