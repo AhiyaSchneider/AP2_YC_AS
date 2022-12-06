2f0b52e76359476eaa48745b1e2f80c83badfcae
Yair Cohen <yaish81@gmail.com>
2022-12-06 19:40:47 +0200

    .

--- KnnCalc.cpp
+++ KnnCalc.cpp
@@ -6,79 +6,62 @@
 #include <sstream>
 #include <string>
 #include <vector>
-#include <list>
 #include "KnnCalc.h"
 #include "DistanceCalc.h"
+#include <utility>
 
 using namespace std;
-//class DistanceCalc;
+// class DistanceCalc;
 /**
  * class KnnCalc will use DistaceCalc to calculate a distance from one vector of data to
  * its K neighbors and conclude its kind accordingly
  */
-    // constructor
-    KnnCalc::KnnCalc(int j, string file, string type)
-    {    
-        //calc already initialized
-        k = j;
-        inputFile = file;
-        distanceType = type;
+// constructor
+KnnCalc::KnnCalc(int j, string file, string type)
+{
+    // calc already initialized
+    k = j;
+    inputFile = file;
+    distanceType = type;
+}
+
+void KnnCalc::findK_NearestNeighbors()
+{
+    ifstream inFile;
+    inFile.open(inputFile);
+    if (!inFile.is_open())
+    { // error openning file
+        cout << "error openning file";
+        exit(1);
     }
-
-
-    void KnnCalc::findK_NearestNeighbors()
+    int v1Length = calc.getV1().size();
+    vector<double> v;
+    string s;
+    while (getline(inFile, s))
     {
-        //vectorList = vector<vector<double>>();
-        //distanceList = vector<double>();
-        ifstream inFile;
-        inFile.open(inputFile);
-        if (!inFile.is_open())
-        { // error openning file
-            cout << "error openning file";
-            exit(1);
-        }
-        // set cin to get input from file
-        /*ifstream in(inputFile);
-        streambuf *cinbuf = cin.rdbuf(); // save old buf
-        cin.rdbuf(in.rdbuf());*/                // redirect std::cin to in.txt!
-        int v1Length = calc.getV1().size();
-        vector<double> v;
-        string s;
-        while (getline(inFile, s))
+        istringstream ss(s);
+        string temp;
+        for (int i = 0; i < v1Length; i++)
         {
-            istringstream ss(s);
-            for (int i = 0; i < v1Length; i++) {
-                string temp;
-                double x;
-                ss >> x;
-                v.push_back(x);
-                if(v.size() == v1Length)
-                {
-                    
-                    ss >> temp;
-                }
-                temp = "";
-                ss.get();
-            }
-            // todo - a function for adding new vector to the list - mabye generalize setV2 func - for extracting a vector from a file regardless to its destination after.
-            //getting the vectros already categories according to type (red/white wine etc.)
-            
-                calc.setV2(v);
-                // TODO - switch case and validation check
-                vectorList.push_back(v); // need to add name of each vector and link the two lists
-                v.clear();
-                distanceList.push_back(calc.manhattan_Distance());
-            
-        }
-        // todo - private partition function for the whole list of vectors extracted from the file
-        // todo - return k first neighbors in the list after partition
-        inFile.close();
-        for (int i = 0; i < distanceList.size(); i++)
-        {
-            cout << distanceList.at(i);
-            cout << "\n";
-            //cout << vectorList.at(i);
-            cout << "\n";
-        }      
+            double x;
+            ss >> x;
+            v.push_back(x);
 
+            ss.get();
+        }
+        //works for unclassified file also?
+        ss >> temp;
+        calc.setV2(v);
+        // TODO - switch case and validation check
+        vectorList.push_back(v);
+        pair <string, double> p;
+        p.first = temp;
+        p.second = calc.manhattan_Distance();
+        distanceList.push_back(p);
+        v.clear();
+        temp = "";
     }
+    // todo - private partition function for the whole list of vectors extracted from the file
+    // todo - return k first neighbors in the list after partition
+    inFile.close();
+}
