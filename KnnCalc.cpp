68b99e5b408e46d5d3aecc694bb7b9b5b162746e
Yair Cohen <yaish81@gmail.com>
2022-12-06 21:56:43 +0200

    with conversion to map for counting types of knn

--- KnnCalc.cpp
+++ KnnCalc.cpp
@@ -9,6 +9,8 @@
 #include "KnnCalc.h"
 #include "DistanceCalc.h"
 #include <utility>
+#include<bits/stdc++.h>
+#include <map>
 
 using namespace std;
 // class DistanceCalc;
@@ -64,4 +66,22 @@ void KnnCalc::findK_NearestNeighbors()
     // todo - private partition function for the whole list of vectors extracted from the file
     // todo - return k first neighbors in the list after partition
     inFile.close();
+    sort(distanceList.begin(), distanceList.end());
+    multimap <string, double> distanceMap;
+    for (int i = 0; i < k; i++)
+    {
+        distanceMap.insert(pair<string, double> (distanceList.at(i).first, distanceList.at(i).second));
+    }
+    pair <int, string> max;
+    max.first = 0;
+    map<string, double> :: iterator it;
+    for (it = distanceMap.begin(); it != distanceMap.end(); it++)
+    {
+        if(max.first < distanceMap.count(it -> first)) {
+            max.first = distanceMap.count(it -> first);
+            max.second = it -> first;
+        }
+    }
+    cout << max.second;
+    
 }
--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include "DistanceCalc.h"
 #include "KnnCalc.h"
 #include <iostream>
+#include <unistd.h>
 using namespace std;
 
 int main() {
-    KnnCalc k(3, "/home/oem/Desktop/AP1/YC-AS2/datasets/iris/iris_classified.csv", "MAN");
+    //char cwd[2048] = "/home/oem/Desktop/AP1/YC-AS2/datasets/iris/iris_classified.csv";
+    //getcwd(cwd, sizeof(cwd));
+    KnnCalc k(10, "/home/oem/Desktop/AP1/YC-AS2/datasets/iris/iris_classified.csv", "MAN");
     //DistanceCalc d;
     //d.printDistances();
     k.findK_NearestNeighbors();
